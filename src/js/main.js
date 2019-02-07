a69f89bd17851dd56c76258de603559216372906
Vadym Semeniuk <vadimsemenykv@gmail.com>
2019-02-07 22:52:37 +0200

    in progress v3

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ function formatSelectionParenthesesWithSpan (selection) {
     if (!selection.id) {
         return selection.text;
     }
-    console.log(selection);
     var match = selection.text.match('\\(.*\\)');
     if (!match) {
         return '<span>' + selection.text + '</span>';
@@ -11,6 +10,7 @@ function formatSelectionParenthesesWithSpan (selection) {
 }
 
 $(document).ready(function() {
+    //SELECTS
     $('.dashboard-panel__user-online').select2({
         placeholder: $('.dashboard-panel__user-online').data('usersOnline'),
         allowClear: true,
@@ -26,4 +26,17 @@ $(document).ready(function() {
         templateSelection: formatSelectionParenthesesWithSpan,
         templateResult: formatSelectionParenthesesWithSpan
     });
+
+    $('.main__show').on('click', function () {
+        var taskContainer = $(this).parent().parent().parent();
+        console.log(taskContainer);
+        console.log(taskContainer.hasClass('main__task'));
+        if (taskContainer.hasClass('main__task_active')) {
+            taskContainer.find('.main__subtasks').hide();
+            taskContainer.removeClass('main__task_active');
+        } else {
+            taskContainer.find('.main__subtasks').show();
+            taskContainer.addClass('main__task_active');
+        }
+    })
 });
