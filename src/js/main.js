c8caf16ce9e8c60ac2ab436ddc3bd354ba06ee87
nataligm <nataligm89@gmail.com>
2019-02-07 20:38:53 +0200

    in progress v1

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
+function formatSelectionWithSpan (selection) {
+    if (!selection.id) {
+        return selection.text;
+    }
+    return $('<div class="main__new-projects">New Projects <span class="main__descending">(descending)</span></div>');
+}
+
 $(document).ready(function() {
     $('.dashboard-panel__user-online').select2({
         placeholder: $('.dashboard-panel__user-online').data('usersOnline'),
         allowClear: true,
         minimumResultsForSearch: -1,
-        dropdownCssClass: 'dashboard-panel__user-online__dropdown',
+        dropdownCssClass: 'select2-dropdown__option',
         width: '173px'
+    });
+    $('.dashboard-panel__user-online-2').select2({
+        placeholder: $('.dashboard-panel__user-online').data('usersOnline'),
+        allowClear: true,
+        minimumResultsForSearch: -1,
+        dropdownCssClass: 'dashboard-panel__user-online__dropdown',
+        width: '173px',
+        templateSelection: formatSelectionWithSpan
     });
 });
