13b8ef3cca447225ba01ff04c7dc2020a6de36a1
nataligm <nataligm89@gmail.com>
2019-02-09 15:18:29 +0200

    upd

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function formatSelectionParenthesesWithSpan (selection) {
+    if (!selection.id) {
+        return selection.text;
+    }
+    var match = selection.text.match('\\(.*\\)');
+    if (!match) {
+        return '<span>' + selection.text + '</span>';
+    }
+    return $('<span>' + match.input.slice(0, match.index) + '<span class="main__projects_search__order">' + match[0] + '</span></span>');
+}
+
+$(document).ready(function() {
+    //SELECTS
+    $('.dashboard-panel__user-online').select2({
+        placeholder: $('.dashboard-panel__user-online').data('usersOnline'),
+        allowClear: true,
+        minimumResultsForSearch: -1,
+        dropdownCssClass: 'select2-dropdown__option dashboard-panel-dropdown__option',
+        width: '173px'
+    });
+    $('.main__projects_search').select2({
+        allowClear: false,
+        minimumResultsForSearch: -1,
+        dropdownCssClass: 'select2-dropdown__option main__search-dropdown__option',
+        width: '229px',
+        templateSelection: formatSelectionParenthesesWithSpan,
+        templateResult: formatSelectionParenthesesWithSpan
+    });
+
+    $('.main__show').on('click', function () {
+        var taskContainer = $(this).parent().parent().parent();
+        if (taskContainer.hasClass('main__task_active')) {
+            taskContainer.find('.main__subtasks').hide();
+            taskContainer.removeClass('main__task_active');
+        } else {
+            taskContainer.find('.main__subtasks').show();
+            taskContainer.addClass('main__task_active');
+        }
+    })
+});
